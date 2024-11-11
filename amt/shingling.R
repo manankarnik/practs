57a69ab10961b37a1063a86dd2cb8ac72a94fe1e
Manan Karnik <manankarnik03@gmail.com>
2024-11-11 23:27:28 +0530

    update

--- amt/shingling.R
+++ amt/shingling.R
@@ -1,15 +1,12 @@
 shingles <- function() {
   n <- readline(prompt="Enter value of k: ")
   k <- as.integer(n)
-  
   text <- readLines("~/college/amt/shingling.txt")
-  text
-  
-  shingle <- vector(mode="character", length=nchar(text) - k + 1)
+  shingle <- vector(mode="character", length=nchar(text) - k)
   
   i = 0
-  while (i < nchar(text) - k + 1) {
-    shingle[i] = substr(text, start=i, stop=i+k)
+  while (i < nchar(text) - k) {
+    shingle[i] <- substr(text, start=i, stop=i+k)
     print(shingle[i])
     i = i + 1
   }
